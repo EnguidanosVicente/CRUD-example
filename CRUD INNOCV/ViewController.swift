ade5c955cff5c3f2aad722a7352e7003810a4a97
Vicente Enguidanos <enguidanosvicente@icloud.com>
2022-09-23 13:06:28 +0100

    Initial Commit

---
+++ CRUD INNOCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CRUD INNOCV
+//
+//  Created by Vicente Enguidanos on 23/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
